415e21ebd50b93ca283d26a059321e0ba89fb8ed
Amira Abdo <amira.abdo@stu.ibu.edu.ba>
2022-06-11 01:20:54 +0200

    idk

---
+++ app/src/main/java/com/example/fooding/MapActivity.java
@@ -0,0 +1,120 @@
+package com.example.fooding;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int LOCATION_REQUEST_CODE = 123;
+    private FusedLocationProviderClient mFueLocationProviderClient;
+    private GoogleMap mMap;
+    private SupportMapFragment mSupportMapFragment;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        mSupportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.main_map);
+        mSupportMapFragment.getMapAsync(this);
+        mFueLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    public void onButtonClick(View view){
+        LatLng markale_farmers_market = new LatLng(43.85901646674545, 18.423402651804448);
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(markale_farmers_market).title("Markale Farmers Market"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(markale_farmers_market));
+    }
+
+    private void getLocation(){
+        if(checkPermissions()){
+            if(isLocationEnabled()){
+                mFueLocationProviderClient.getLastLocation().addOnCompleteListener(
+                        new OnCompleteListener<Location>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Location> task) {
+                                Location location = task.getResult();
+                                if(location != null){
+                                    LatLng currentLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                                    mMap.clear();
+                                    mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker on current location"));
+                                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15.0f));
+                                }else{
+                                    Toast.makeText(MapActivity.this,"Unable to get location",Toast.LENGTH_LONG).show();
+                                }
+
+                            }
+                        }
+                );
+            }else {
+                Toast.makeText(this,"Location is disabled",Toast.LENGTH_LONG).show();
+            }
+        }else {
+            getPermissions();
+        }
+    }
+
+    private boolean isLocationEnabled(){
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Markale Farmers Market and move the camera
+        LatLng markale_farmers_market = new LatLng(43.85901646674545, 18.423402651804448);
+        mMap.addMarker(new MarkerOptions().position(markale_farmers_market).title("Markale Farmers Market"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(markale_farmers_market));
+    }
+
+    private boolean checkPermissions(){
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        return false;
+    }
+
+    private void getPermissions(){
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},
+                LOCATION_REQUEST_CODE
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == LOCATION_REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // have permission to use location
+                getLocation();
+            }else{
+                Toast.makeText(this,"Permission not accepted",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fooding/MapActivity2.java
@@ -0,0 +1,47 @@
+package com.example.fooding;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity2 extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map2);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng markale_farmers_market = new LatLng(43.85901646674545, 18.423402651804448);
+        mMap.addMarker(new MarkerOptions().position(markale_farmers_market).title("Marker at Markale farmers market"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(markale_farmers_market));
+    }
+}
